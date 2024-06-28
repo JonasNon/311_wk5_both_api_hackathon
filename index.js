9ab8e8d04454b822bca7258b22fd8ff516e5cdc3
cyberkatrina <katrinaaliashkevich@utexas.edu>
2024-06-27 19:08:53 -0500

    express set up complete, server running

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('re-testing')
+const express = require("express");
+const bodyParser = require("body-parser");
+// const usersRouter = require('./routers/users');
+
+const app = express();
+const port = process.env.PORT || 4001;
+
+app.use(bodyParser.json())
+// app.use('/users', usersRouter)
+
+app.get('/', (req, res) => {
+  res.send('Welcome to our server!')
+})
+
+app.listen(port, () => {
+ console.log(`Web server is listening on port ${port}!`);
+});

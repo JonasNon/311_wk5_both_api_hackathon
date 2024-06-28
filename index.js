02aed6e6104a3212b9b44d3cd31c0a4238d33f2a
eflaatten <elliotflaatten@live.com>
2024-06-27 20:07:13 -0500

    set up router file

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-// const usersRouter = require('./routers/users');
+const employeesRouter = require('./routes/employees')
 
 const app = express();
 const port = process.env.PORT || 4001;
 
 app.use(bodyParser.json())
-// app.use('/users', usersRouter)
+app.use('/employees', employeesRouter)
 
 app.get('/', (req, res) => {
   res.send('Welcome to our server!')

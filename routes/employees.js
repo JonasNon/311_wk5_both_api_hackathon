9921e7adaa57df8735fdfb60cafcb2b05eb58623
JonasNon <officialjonasn@gmail.com>
2024-06-27 20:43:57 -0500

    added sql functions for get all employees and by id

--- routes/employees.js
+++ routes/employees.js
@@ -3,5 +3,7 @@ const router = require('express').Router();
 const employeesController = require('../controller/employees')
 
 router.get('/', employeesController.getEmployees)
+router.get('/:id', employeesController.getEmployeeById)
+
 
 module.exports = router;

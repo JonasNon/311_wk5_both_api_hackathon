df7cbe424a8ec5d7e49cd88153275e2f95cd6bbb
eflaatten <elliotflaatten@live.com>
2024-06-27 20:28:13 -0500

    added router for controller func

--- routes/employees.js
+++ routes/employees.js
@@ -2,4 +2,6 @@ const router = require('express').Router();
 
 const employeesController = require('../controller/employees')
 
+router.get('/', employeesController.getEmployees)
+
 module.exports = router;
